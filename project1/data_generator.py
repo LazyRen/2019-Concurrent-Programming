e72ff2873b7f5dadd3645c517c42adfb09ef9e97
DaeIn Lee <lazy0ren@gmail.com>
2019-09-18 14:36:29 +0900

    made data_generator to simulate large file test

---
+++ project1/data_generator.py
@@ -0,0 +1,23 @@
+from random import shuffle
+
+
+class DataTuple:
+    def __init__(self, key):
+        self.key     = key
+        self.payload = "  " + key * 8 + "0000000"
+
+
+def runTest(outputFile, totalTuple):
+    dataList = []
+    for i in range(1, totalTuple+1):
+        dataList.append(DataTuple(str(i).zfill(10)))
+    shuffle(dataList)
+    with open(outputFile, 'w') as writeFile:
+        for t in dataList:
+            writeFile.write(t.key + t.payload + '\n')
+    print("writing to " + outputFile + "is done\n")
+
+
+if __name__ == "__main__":
+    runTest("input_1GB.test", (int)((1024**3)/100))
+    runTest("input_2GB.test", (int)(2*(1024**3)/100))
