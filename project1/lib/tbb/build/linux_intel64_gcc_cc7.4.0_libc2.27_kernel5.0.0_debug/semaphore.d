08c2425bb5dd895861a0d62ccf7100f77d7ce60a
DaeIn Lee <daein0lee@gmail.com>
2019-09-23 23:16:40 +0900

    remove intel tbb(cannot even compile it...)

--- project1/lib/tbb/build/linux_intel64_gcc_cc7.4.0_libc2.27_kernel5.0.0_debug/semaphore.d
+++
@@ -1,7 +0,0 @@
-semaphore.o: ../../src/tbb/semaphore.cpp ../../src/tbb/semaphore.h \
- ../../include/tbb/atomic.h ../../include/tbb/tbb_machine.h \
- ../../include/tbb/tbb_stddef.h ../../include/tbb/tbb_config.h \
- ../../include/tbb/machine/gcc_generic.h \
- ../../include/tbb/machine/gcc_ia32_common.h \
- ../../include/tbb/machine/gcc_itsx.h \
- ../../include/tbb/machine/linux_common.h ../../include/tbb/tbb_stddef.h
